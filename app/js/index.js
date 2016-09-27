2466c4a03e6d4f073c039808db0cf60122330143
JailBreak <admin@vgee.cn>
2016-09-27 12:04:06 +0800

    添加注释，优化代码结构，更改血量显示

--- app/js/index.js
+++ app/js/index.js
@@ -2,10 +2,11 @@ var gameController = {
     _animation: null,
     _canvasWidth: 0,
     _canvasHeight: 0,
-    _floorWidth: $('.canvas').width() / 6,
+    _floorWidth: $('.canvas').width() / 5,
     _floorDeltaY: 50,
     _floorScore: 1,
     _speed: 50, //pixel per second
+    _maxSpeed: 200,
     _blood: 12,
     _$canvas: $('.canvas'),
     _$scroller: $('.scroller'),
@@ -16,7 +17,7 @@ var gameController = {
     _peopleWidth: $('.people').width(),
     _scrollerHeight: $('.scroller').height(),
     __currentScrollerY: 0,
-    __currentPeopleY: 1,
+    __currentPeopleY: 20,
     __currentPeopleVertical: 0,
     __floorScrollerY: 200,
     __frameIndex: 0,
@@ -31,15 +32,17 @@ var gameController = {
         var _top = this.__floorScrollerY += this._floorDeltaY,
             //楼梯横向位置随机出现
             _left = Math.random() * (this._canvasWidth - this._floorWidth);
+
         var floors = [
             '<i class="floor normal"></i>',
+            '<i class="floor spring"></i>',
             '<i class="floor weak"></i>',
             '<i class="floor scroll-left"></i>',
             '<i class="floor scroll-right"></i>',
             '<i class="floor nail"></i>'
         ]
-        //新建楼梯，并添加到卷轴中去
-        $(floors[Math.floor(Math.random() * 5)]).css({
+        //随机新建楼梯，并添加到卷轴中去
+        $(floors[Math.floor(Math.random() * floors.length)]).css({
             top: _top,
             left: _left,
             width: this._floorWidth
@@ -49,6 +52,11 @@ var gameController = {
         $('.floor').eq(0).remove();
         this._floorScore++;
         $('.floor-count').text(this._floorScore)
+    },
+    updateBlood: function() {
+        var __blood = '★★★★★★★★★★★★☆☆☆☆☆☆☆☆☆☆☆☆';
+        __blood = __blood.slice(12 - this._blood, 12*2 - this._blood);
+        $('.blood').text(__blood);
     },
     loseBlood: function() {
         //当人物在平台上时，不重复扣血
@@ -61,7 +69,7 @@ var gameController = {
             this.gameover();
             return;
         }
-        $('.blood').text(this._blood);
+        this.updateBlood()
     },
     addBlood: function() {
         //当人物在平台上时，或者血量大于12，不重复加血
@@ -69,7 +77,7 @@ var gameController = {
             return;
         }
         this._blood += 1;
-        $('.blood').text(this._blood);
+        this.updateBlood()
     },
     floorNormal: function() {
         this.addBlood();
@@ -77,29 +85,33 @@ var gameController = {
     floorNail: function() {
         this.loseBlood();
     },
-    floorWeak: function($ele) {
-        var _this = this;
+    floorWeak: function($floorEle) {
         this.addBlood();
-        $ele.addClass('over');
+        $floorEle.addClass('over');
+        //短暂停留后，标记该元素可强行穿过
         setTimeout(function() {
-            //标记该元素强行穿过
-            $ele[0].cross = true;
+            $floorEle[0].cross = true;
         }, 200);
     },
     floorScroll: function(direction) {
         this.addBlood();
     },
+    floorSpring: function() {
+        this.addBlood();
+    },
     people: function(fps) {
         //人物纵向每帧移动距离
         var _deltaPeopleY = this._peopleSpeed / fps;
+        //卷轴纵向每帧移动距离
+        var _deltaY = this._speed / fps;
         //人物横向每帧移动距离
         var _deltaPeopleVertical = this._peopleVerticalSpeed / fps;
         //缓存floor
         var $floor = $('.floor');
         //缓存offset
         var peopleOffset = this._$people.offset();
+        //人物掉落屏幕下方，游戏结束
         if(peopleOffset.top > this._canvasHeight) {
-            // alert('Game over!');
             this.gameover();
             return
         }
@@ -107,31 +119,38 @@ var gameController = {
         for(i = 0; i < $floor.length; i++) {
             //缓存offset
             var floorOffset = $floor.eq(i).offset();
+            //人物与楼梯纵向距离
             var distanceGap = Math.abs(peopleOffset.top + this._peopleHeight - floorOffset.top);
-            if(peopleOffset.top <= 0) {
-                this.loseBlood();
+            //当人物撞到顶部，掉血+掉落
+            if(peopleOffset.top <= _deltaPeopleY + _deltaY) {
                 this.__onFloor = false;
+                this.loseBlood();
                 break;
             }
-            // console.log(this._forceCross);
+
+                //元素不可直接穿过
             if( !$floor.eq(i)[0].cross &&
-                distanceGap <= 10 && 
+                //人物与楼梯纵向距离在一帧移动距离之内
+                distanceGap <= _deltaPeopleY + _deltaY && 
+                //人物横向距离不超过楼梯最左
                 peopleOffset.left > floorOffset.left - this._peopleWidth && 
+                //人物横向距离不超过楼梯最右
                 peopleOffset.left < floorOffset.left + this._floorWidth ) {
-                //人物与楼梯偏差修正，并立即更新视图
+                //人物与楼梯偏差修正
                 this.__currentPeopleY = floorOffset.top - this._peopleHeight + 2;
-                this._$people.css({
-                    transform: 'translate3d(' + this.__currentPeopleVertical + 'px , ' + this.__currentPeopleY + 'px ,0)'
-                });
-                //卷轴纵向每帧移动距离
-                _deltaY = this._speed / fps,
-                //让人物随着楼梯共同向上移动
+                //立即更新视图
+                this.peopleUpdateView();
+                //让人物随着楼梯共同向上移动（站在楼梯上效果）
                 this.__currentPeopleY -= _deltaY;
+                //施加各类楼梯特殊属性
                 if($floor.eq(i).hasClass('normal')) {
                     this.floorNormal();
                 }
                 if($floor.eq(i).hasClass('nail')) {
                     this.floorNail();
+                }
+                if($floor.eq(i).hasClass('spring')) {
+                    this.floorSpring();
                 }
                 if($floor.eq(i).hasClass('weak')) {
                     this.floorWeak($floor.eq(i));
@@ -169,6 +188,11 @@ var gameController = {
             }
         }
 
+        //更新人物视图
+        this.peopleUpdateView();
+    },
+    //更新人物视图
+    peopleUpdateView: function() {
         //设定人物位置, translate3d开启GPU加速，消除抖动
         this._$people.css({
             transform: 'translate3d(' + this.__currentPeopleVertical + 'px , ' + this.__currentPeopleY + 'px ,0)'
@@ -198,7 +222,7 @@ var gameController = {
                 _this._peopleGoLeft = false;
                 return;
             }
-        });;
+        });
 
         $('.controller .left-ct').on('touchstart', function(ev) {
             _this._peopleGoRight = false;//预防按键同时按下的冲突情况 
@@ -225,10 +249,13 @@ var gameController = {
         //计算卷轴位置
         this.__currentScrollerY -= _deltaY;
 
-        //位置reset
+        //当卷轴超出一定长度之后，进行位置reset、缩减长度，防止Crash现象
         if(this.__currentScrollerY <= -this._canvasHeight * 2) {
+            //将卷轴滚动高度减小一屏
             this.__currentScrollerY += this._canvasHeight;
+            //将楼梯偏移高度减小一屏
             this.__floorScrollerY -= this._canvasHeight;
+            //重置现有楼梯位置
             var $floor = $('.floor');
             for(i = 0; i < $floor.length; i++) {
                 $floor.eq(i).css({
@@ -251,7 +278,7 @@ var gameController = {
         //调用人物渲染
         this.people(fps);
         // 越来越high
-        if(this._speed <= 200) {
+        if(this._speed <= this._maxSpeed) {
             this._speed += 0.05;
         }
 
@@ -296,10 +323,10 @@ var gameController = {
         this.peopleUserController();
         //人物位置预设
         this.__currentPeopleVertical = this._canvasWidth/2 + this._peopleWidth/2;
-        this._$people.show();
-        this._$people.css({
-            transform: 'translate3d(' + this.__currentPeopleVertical + 'px , ' + this.__currentPeopleY + 'px ,0)'
-        });
+        //首次更新人物视图
+        this.peopleUpdateView();
+        //首次更新人物血量
+        this.updateBlood();
         //以每秒60帧执行游戏动画
         this.run(60);
     }
